8c94c42f9e6c9b4e9fd1ea0e522fd472a537cb57
franklin <frank.fkn.23@gmail.com>
2025-07-07 12:04:05 -0300

    exercicio aula 02 - ep para retornar html

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -8,3 +9,20 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello():
+    return """
+    <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hello</title>
+</head>
+<body>
+	<h1>Hello World!!!</h1>
+</body>
+</html>
+"""
--- tests/test_fastapi_zero.py
+++ tests/test_fastapi_zero.py
@@ -12,3 +12,25 @@ def test_root():
 
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_hello_http():
+    client = TestClient(app)
+    response = client.get('/hello')
+    assert (
+        response.text
+        == """
+    <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hello</title>
+</head>
+<body>
+	<h1>Hello World!!!</h1>
+</body>
+</html>
+"""
+    )
+    assert response.status_code == HTTPStatus.OK

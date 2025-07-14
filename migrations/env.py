842272b37c66055236a1494276115507a89425e5
franklin <frank.fkn.23@gmail.com>
2025-07-13 22:21:52 -0300

    deixando migracao assincrona

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,7 @@
+import asyncio
 from logging.config import fileConfig
 
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -52,28 +54,33 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection, target_metadata=target_metadata
+    )
+    with context.begin_transaction():
+        context.run_migrations()
 
-def run_migrations_online() -> None:
+
+async def run_async_migrations() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
-        with context.begin_transaction():
-            context.run_migrations()
 
+def run_migrations_online() -> None:
+    asyncio.run(run_async_migrations())
 
 if context.is_offline_mode():
     run_migrations_offline()
